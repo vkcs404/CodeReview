b3fe4bb3ab6560d685e91e812d8ff8d18a3b0575
Vitor Santos <vitor.vkcs31@gmail.com>
2026-04-16 10:32:52 -0300

    Estrutura Inicial

--- solucoes.py
+++ solucoes.py
@@ -1,1 +1,12 @@
+def sao_anagramas(string1, string2):
+  # Implemente a logica
+  pass
+
+def cifra_de_cesar(texto, deslocamento):
+  # Implemente a logica
+  pass
+
+def valida_cpf(cpf_string):
+  # Implemente a logica
+  pass
 
