7d9e6eacd04413d04f9927fbdadc1a9704bfc86b
Andr3-Rez3nd3 <andresantosderezende@gmail.com>
2026-04-16 10:50:36 -0300

    correcao do comentario para ser mais explicito

--- solucoes.py
+++ solucoes.py
@@ -1,11 +1,11 @@
 def sao_anagramas(string1, string2):
-  #Remove os espaços e converte para minúsculas, depois ordena os caracteres
+  #A função remove os espaços e converte as letras para minúsculas, depois reordena os caracteres
   s1 = sorted(string1.replace(" ", "").lower())
   s2 = sorted(string2.replace(" ", "").lower())
   return s1 == s2
 
 def cifra_de_cesar(texto, deslocamento):
-  # Implemente a logica
+# Implemente a lógica
   pass
 
 def encontrar_maior_palavra(frase):
