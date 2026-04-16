08364f67a27690e7184afeea42798bdbc4c4af7b
Andr3-Rez3nd3 <andresantosderezende@gmail.com>
2026-04-16 10:43:41 -0300

    implementacao das funcoes de anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,6 +1,8 @@
 def sao_anagramas(string1, string2):
-  # Implemente a logica
-  pass
+  #Remove os espaços e converte para minúsculas, depois ordena os caracteres
+  s1 = sorted(string1.replace(" ", "").lower())
+  s2 = sorted(string2.replace(" ", "").lower())
+  return s1 == s2
 
 def cifra_de_cesar(texto, deslocamento):
   # Implemente a logica
